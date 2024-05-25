424ed7855be60924e12ac3fd8204a9c497fded49
Karandeep Randhawa <karandeeprandhawa27@gmail.com>
2024-05-25 18:08:39 +0530

    Error resolve

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-import reactRefresh from '@vitejs/plugin-react-refresh';
-import { defineConfig } from 'vite';
-import styledComponents from 'vite-plugin-styled-components';
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), styledComponents()],
-});
+  plugins: [react()],
+})
+
+
